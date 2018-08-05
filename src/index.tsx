bd84e1a52a6750722b6c0353f128dd006c100aa4
Enes YILMAZ <enisyilmaz12@gmail.com>
2018-08-05 19:46:30 +0300

    added sass configurtion

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 import { render } from 'react-dom';
 
 import App from './components/App';
+import '../public/styles/main.scss';
 
+const rootElement = document.getElementById('root');
 
 render(
     <App/>,
-    document.getElementById('root')
+    rootElement
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: { main: './src/index.tsx' },
     output: {
-        filename: '[name].bundle.js',
+        filename: '[name].[hash].js',
         path: path.resolve(__dirname, 'dist')
     },
     devtool: 'inline-source-map',
@@ -21,13 +24,28 @@ module.exports = {
                 test: /\.(ts|tsx)$/,
                 loader: 'ts-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
             }
         ]
     },
     plugins: [
+        new CleanWebpackPlugin('dist', {}),
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css'
+        }),
         new HtmlWebpackPlugin({
             template: './public/index.html',
             filename: 'index.html'
-        })
+        }),
+        new WebpackMd5Hash()
     ]
 }
